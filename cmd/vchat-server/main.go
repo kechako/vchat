3a8d5259bc1583ae2669419af25c7eb4adff2bff
Ryosuke Akiyama <r@554.jp>
2018-01-30 22:46:53 +0900

    Revert "Fix server"
    This reverts commit 0372b17a33c00cef601254eb8768663e19203ed9.

--- cmd/vchat-server/main.go
+++ cmd/vchat-server/main.go
@@ -139,7 +139,7 @@ func (r *Room) run() {
 
 			r.clients.Range(func(key, value interface{}) bool {
 				c := value.(*Client)
-				r.conn.WriteTo(buf, c.Addr)
+				r.conn.WriteToUDP(buf, c.Addr)
 				return true
 			})
 		}
